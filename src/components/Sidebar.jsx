7d769d2d395fc4f5dd0e482a05c83c5ba3fa94c3
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-09-05 08:58:56 -0300

    Create component Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -10,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img 
-          src="https://github.com/raphaelfontoura.png" 
-          className={styles.avatar}
-        />
+        <Avatar src="https://github.com/raphaelfontoura.png" />
         <strong>Raphael Fontoura</strong>
         <span>Fullstack developer</span>
       </div>
